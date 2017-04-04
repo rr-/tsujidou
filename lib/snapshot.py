68e354d62a81871de91bea650152db9fc60d41ba
rr- <rr-@sakuya.pl>
2017-04-04 20:44:48 +0200

    unpack: add snapshots

---
+++ lib/snapshot.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from typing import List, Any
+from lib import engine
+
+
+class SnapshotEntry:
+    def __init__(
+            self,
+            file_type: engine.FileType,
+            file_name: str,
+            file_name_hash: int,
+            relative_path: Path,
+            stat: Any) -> None:
+        self.file_type = file_type
+        self.file_name = file_name
+        self.file_name_hash = file_name_hash
+        self.relative_path = relative_path
+        self.stat = stat
