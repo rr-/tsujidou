3a921cb23accf6f6a9cea0aca281415a001b1564
rr- <rr-@sakuya.pl>
2017-04-07 23:38:02 +0200

    unpack: add concurrency

--- lib/util.py
+++ lib/util.py
@@ -1,7 +1,12 @@
+import threading
 from pathlib import Path
 
 
+_lock = threading.Lock()
+
+
 def save_file(path: Path, content: bytes) -> None:
-    path.parent.mkdir(parents=True, exist_ok=True)
+    with _lock:  # mkdir doesn't seem to be thread safe
+        path.parent.mkdir(parents=True, exist_ok=True)
     with path.open('wb') as handle:
         handle.write(content)
