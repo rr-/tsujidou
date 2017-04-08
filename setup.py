b611b9ee77d70608f3ba718800447482fa8181be
rr- <rr-@sakuya.pl>
2017-04-08 12:41:24 +0200

    build: fix extension namespaces

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 
 setup(ext_modules=[
-    Extension('lib.tlg5', sources = ['ext/tlg5.c', 'ext/stream.c', 'ext/lzss.c']),
-    Extension('lib.tlg6', sources = ['ext/tlg6.c', 'ext/stream.c', 'ext/lzss.c']),
+    Extension('lib.tlg.tlg5', sources=['ext/tlg5.c', 'ext/stream.c', 'ext/lzss.c']),
+    Extension('lib.tlg.tlg6', sources=['ext/tlg6.c', 'ext/stream.c', 'ext/lzss.c']),
 ])
