7648b010077d4f016ef1a2e54bd146ca136d601e
rr- <rr-@sakuya.pl>
2017-04-14 19:33:29 +0200

    lib: add word wrapping

--- lib/script.py
+++ lib/script.py
@@ -1,4 +1,6 @@
 import re
+import sys
+from pathlib import Path
 from typing import List, Iterator
 
 
@@ -7,8 +9,17 @@ def decode_script(content: bytes) -> bytes:
         return '{} {}'.format(command, arg or '').rstrip()
 
     def process_line(line: str) -> List[str]:
+        if not line:
+            return ['']
+
+        match = re.match('^【】(?P<text>.*)$', line)
+        if match:
+            ret = []
+            ret.append(format_command('TEXT', match.group('name')))
+            return ret
+
         match = re.match(
-            '【(?P<name>[^,]*)(?:,(?P<sound>.+))?】(?P<text>.*)',
+            '^【(?P<name>[^,]*)(?:,(?P<sound>.+))?】(?P<text>.*)$',
             line)
         if match:
             ret = []
@@ -18,9 +29,6 @@ def decode_script(content: bytes) -> bytes:
             ret.append(format_command('SPEAK-ORIG', match.group('text')))
             ret.append(format_command('SPEAK-TEXT', match.group('text')))
             return ret
-
-        if not line:
-            return ['']
 
         if ord(line[0]) < 128:
             return [format_command('CODE', line)]
@@ -38,11 +46,60 @@ def decode_script(content: bytes) -> bytes:
     return '\n'.join(lines).encode('utf-8')
 
 
-def encode_script(content: bytes) -> bytes:
+def encode_script(script_path: Path, content: bytes) -> bytes:
+    def process_text(input_string: str, line_number: int) -> str:
+        MAX_LINE_COUNT = 3
+        MAX_LINE_LENGTH = 49
+        MAX_TEXT_LENGTH = 80
+        NEWLINE_MARKER = '[n]'
+
+        output_lines = []
+        for input_line in input_string.split(NEWLINE_MARKER):
+            if input_line:
+                line_length = 0
+                output_words = []
+                for word in input_line.split():
+                    if line_length + len(word) + 1 <= MAX_LINE_LENGTH:
+                        output_words.append(word)
+                        line_length += len(word) + 1
+                    else:
+                        output_lines.append(' '.join(output_words))
+                        output_words = [word]
+                        line_length = len(word)
+                if output_words:
+                    output_lines.append(' '.join(output_words))
+            else:
+                output_lines.append('')
+        output_string = NEWLINE_MARKER.join(output_lines)
+
+        if len(output_lines) > MAX_LINE_COUNT:
+            print(
+                'Warning: too many lines in {} at line {}'.format(
+                    script_path, line_number),
+                file=sys.stderr)
+
+        if any(len(line) > MAX_LINE_LENGTH for line in output_lines):
+            print(
+                'Warning: too long line in {} at line {}'.format(
+                    script_path, line_number),
+                file=sys.stderr)
+
+        if len(output_string.replace(NEWLINE_MARKER, '')) > MAX_TEXT_LENGTH:
+            print(
+                'Warning: too many characters in {} at line {} ({})'.format(
+                    script_path,
+                    line_number,
+                    output_string.replace(NEWLINE_MARKER, '')
+                    [MAX_TEXT_LENGTH:]),
+                file=sys.stderr)
+
+        return output_string
+
     def gather_lines() -> Iterator[str]:
         speech = {}
 
-        for old_line in content.decode('utf-8').split('\n'):
+        for i, old_line in enumerate(content.decode('utf-8').split('\n')):
+            line_number = i + 1
             if not old_line:
                 yield ''
                 continue
@@ -61,17 +118,17 @@ def encode_script(content: bytes) -> bytes:
                 continue
             elif command == 'SPEAK-TEXT':
                 assert speech is not None
-                speech['text'] = args
+                speech['text'] = process_text(args, line_number)
                 if 'file' in speech:
-                    yield '【{char},{file}】あ{text}'.format(**speech)
+                    yield '【{char},{file}】{text}'.format(**speech)
                 else:
-                    yield '【{char}】あ{text}'.format(**speech)
+                    yield '【{char}】{text}'.format(**speech)
                 speech = {}
             elif command == 'CODE':
                 yield args
             elif command == 'ORIG':
                 continue
             elif command == 'TEXT':
-                yield 'あ{text}'.format(text=args)
+                yield '【】' + process_text(args, line_number)
 
     return '\n'.join(gather_lines()).encode('cp932')
